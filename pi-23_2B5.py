53e411d675589b8b45bf68d89996790365c10840
mariogrim <143227972+mariogrim@users.noreply.github.com>
2023-10-18 15:38:18 +0200

    Add files via upload

---
+++ pi-23_2B5.py
@@ -0,0 +1,20 @@
+
+# CODE:5
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ingrese dos palabras y arme combinaciones con ella
+palabra_1 = str(input('Ingrese palabra 1:'))
+
+palabra_2 = str(input('Ingrese palabra 2:'))
+
+# Alumno:
+# Crear una variable llamada palabra_combinada
+# con los recortes solicitados de las variables
+# palabra_1 y palabra_2 en el orden correspondiente
+
+
+# Imprima en pantalla la variable palabra_combinada
+
+palabra_combinada = palabra_1[0:3] + palabra_2[0:2]
+print ("La palabra combinada es:", palabra_combinada)
